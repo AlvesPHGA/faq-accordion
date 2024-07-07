e77d8adcc33a466988aa7223fdef40e13e390ee9
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-07-07 15:38:47 -0300

    feat: add a feat to main component with named main

---
+++ main.js
@@ -0,0 +1,42 @@
+class Main extends HTMLElement {
+   constructor() {
+      super();
+      this.build();
+   }
+
+   build() {
+      const shadow = this.attachShadow({ mode: 'open' });
+
+      shadow.appendChild(this.styles());
+
+      const main = this.createMain();
+
+      shadow.appendChild(main);
+   }
+
+   createMain() {
+      const main = document.createElement('main');
+
+      main.classList.add('main-component');
+
+      return main;
+   }
+
+   styles() {
+      const style = document.createElement('style');
+
+      style.textContent = `
+         .main-component{
+            width: 50vw;
+            height: 50vh;
+            border-radius: 2.5rem;
+            background: white;
+            margin: auto;
+         }
+      `;
+
+      return style;
+   }
+}
+
+customElements.define('main-component', Main);
